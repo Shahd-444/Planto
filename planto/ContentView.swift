b36871225f7a97f17e37ca8199a3191b5df165b4
Shahd <Shahd4.mu21@gmail.com>
2025-10-18 20:26:44 +0300

    Initial Commit

---
+++ planto/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  planto
+//
+//  Created by Shahd Muharrq on 26/04/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ planto/plantoApp.swift
@@ -0,0 +1,17 @@
+//
+//  plantoApp.swift
+//  planto
+//
+//  Created by Shahd Muharrq on 26/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct plantoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
